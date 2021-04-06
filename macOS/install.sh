d949c244354d5bc13a89683033a75a5fb58c6b0b
Alex Jellinger <alexjellinger@Alexs-MacBook-Pro.local>
2021-04-06 16:16:51 -0400

    Add brew to installer
    Installs brew if it isn't yet
    Upgrades and updates brew
    Installs openocd and dfu-util

--- macOS/install.sh
+++ macOS/install.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
+
+# path variable
+echo "Installing DaisyToolchain"
+
 MACOS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 MACOS="$MACOS"/bin
 echo -e "\nexport PATH="$MACOS:'$PATH'"" >> ~/.bashrc
 echo -e "\nexport PATH="$MACOS:'$PATH'"" >> ~/.zshrc
+
+# install brew
+if ! command -v brew &> /dev/null
+then
+    echo "Installing Homebrew: Follow onscreen instructions"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+#upgrade homebrew
+echo "Upgrading Homebrew"
+brew update
+brew upgrade
+
+# install packages
+echo "Installing openocd"
+brew install openocd
+
+echo "Installing dfu-util"
+brew install dfu-util
