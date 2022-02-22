6c9365f38e216d7ddafa8d59f3aa225f4be9c37e
CorvusPrudens <corvusprudens@gmail.com>
2022-02-21 17:41:14 -0700

    Removed .pkg since it'll be too big anyway

--- macOS/easy-build.sh
+++ macOS/easy-build.sh
@@ -3,5 +3,6 @@ name=DaisyToolchain
 
 ./build-macos-x64.sh $name $version
 
+mkdir -p bin
 cp target/pkg/$name-macos-installer-x64-$version.pkg bin
 echo "Generated $name-macos-installer-x64-$version.pkg in bin folder"
